d8b9fa0d6df9f1dfadbb96770a88bc5edbe83bc4
Chandra Gopalaiah <chandragopalaiah@gmail.com>
2016-12-10 11:17:01 -0800

    Cleaning up Angular - Breaking Angular into Services

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,22 @@
 var app = angular.module("app", [])
 
-app.controller("PostsController", function($scope, $http) {
-    $http.get("/api/posts")
-        .success(function (posts) {
-            $scope.posts = posts
-        })
+app.service('PostsService', function ($http) {
+    this.fetch = function() {
+        return $http.get('/api/posts')
+    }
+    this.create = function(post) {
+        return $http.post('/api/posts', post)
+    }
+})
+
+app.controller("PostsController", function($scope, PostsService) {
+    PostsService.fetch().success(function (posts) {
+        $scope.posts = posts
+    })
 
     $scope.addPost = function () {
         if ($scope.postBody) {
-            $http.post("/api/posts", {
+            PostsService.create({
                 username: "jayplabs",
                 body: $scope.postBody
             }).success(function (post) {
--- controllers/static.js
+++ controllers/static.js
@@ -6,6 +6,6 @@ router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../layouts", "posts.html"))
 })
 
-router.use(express.static(__dirname + "/../assets"))
+router.use(express.static(path.join(__dirname, "/../assets")))
 
 module.exports = router
