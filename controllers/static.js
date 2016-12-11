7107ef7118ea3a1563273bfd68ac6f612190efc5
Chandra Gopalaiah <chandragopalaiah@gmail.com>
2016-12-10 16:24:56 -0800

    added router

--- controllers/static.js
+++ controllers/static.js
@@ -2,10 +2,11 @@ var express = require('express')
 var router = express.Router()
 var path = require('path')
 
+router.use(express.static(path.join(__dirname, '/../assets')))
+router.use(express.static(path.join(__dirname, '/../templates')))
+
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../layouts', 'app.html'))
 })
-
-router.use(express.static(path.join(__dirname, '/../assets')))
 
 module.exports = router
