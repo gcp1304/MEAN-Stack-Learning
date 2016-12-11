26aef6de0f9a6f3da795b17da2946beafa8c0c64
Chandra Gopalaiah <chandragopalaiah@gmail.com>
2016-12-10 17:26:58 -0800

    Displaying logged in username

--- ng/login.controller.js
+++ ng/login.controller.js
@@ -2,8 +2,8 @@ angular.module('app')
 .controller('LoginController', function ($scope, UserService) {
     $scope.login = function(username, password) {
         UserService.login(username, password)
-        .then(function (user) {
-            console.log(user)
+        .then(function (response) {
+            $scope.$emit('login', response.data)
         })
     }
 })
