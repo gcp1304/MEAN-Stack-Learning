7a3aa24a7f654aacbc073d84187d5ac6112d5e70
Chandra Gopalaiah <chandragopalaiah@gmail.com>
2016-12-10 19:59:35 -0800

    redirect after login

--- ng/login.controller.js
+++ ng/login.controller.js
@@ -1,9 +1,10 @@
 angular.module('app')
-.controller('LoginController', function ($scope, UserService) {
+.controller('LoginController', function ($scope, UserService, $location) {
     $scope.login = function(username, password) {
         UserService.login(username, password)
         .then(function (response) {
             $scope.$emit('login', response.data)
+            $location.path('/')
         })
     }
 })
